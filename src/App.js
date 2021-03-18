27969473837cd6084b3c432d27e056b902411484
BetterMe <betterme@lzahorui-air13.local>
2021-03-18 14:37:43 +0200

    Button was created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import './App.scss';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Test from './components/test';
+// import Test from './components/test'
+import Discount from './components/Discount/Discount';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Test />
+        <Discount />
       </div>
     </Provider>
   );
