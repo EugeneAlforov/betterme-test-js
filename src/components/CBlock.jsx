63e629a7692fb0a8be9f47b3e25ca576bd09f2fc
Dima Diachenko <32308286+zarg129@users.noreply.github.com>
2021-03-18 13:04:42 +0200

    wrap

--- src/components/CBlock.jsx
+++ src/components/CBlock.jsx
@@ -1,4 +1,5 @@
 import recat, {useState, useEffect} from 'react';
+import Timer from './Timer';
 
 const CBlock = () => {
 
@@ -7,7 +8,7 @@ const CBlock = () => {
     <div className="cblock">
       <h1 className="cblock__title">Reserved<span className="cblock__subtitle"> 75% discount</span>for:</h1>
       <div className="cblock__timer">
-
+    <Timer />
       </div>
     </div>
   )
