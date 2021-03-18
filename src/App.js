1fefd4328dd5055aa6693c90741b8effef89947f
Dima Diachenko <32308286+zarg129@users.noreply.github.com>
2021-03-18 12:54:42 +0200

    wrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import './App.scss';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Test from './components/test';
+import CBlock from './components/CBlock.jsx';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Test />
+        <div className="wrapper">
+          <CBlock />
+        </div>
+        
       </div>
     </Provider>
   );
---
+++ src/components/CBlock.jsx
@@ -0,0 +1,16 @@
+import recat, {useState, useEffect} from 'react';
+
+const CBlock = () => {
+
+
+  return(
+    <div className="cblock">
+      <h1 className="cblock__title">Reserved<span className="cblock__subtitle"> 75% discount</span>for:</h1>
+      <div className="cblock__timer">
+
+      </div>
+    </div>
+  )
+};
+
+export default CBlock;
