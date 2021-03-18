8c6376661591f6d3b7dbdbc34ac9d8a7e8a87c73
Eugene Alforov <eugenalforov@gmail.com>
2021-03-18 14:57:24 +0200

    Added header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Main from './components/Main';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Header />
         <Main />
       </div>
     </Provider>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <img className="header-logo" src="fasting.png" />
+      <img className="header-sandwichButton" src="sandwich-button.png" />
+    </header>
+  );
+}
+
+
+export default Header;
