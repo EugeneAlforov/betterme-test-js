65fb2ccbc191fef7c88724efa6145125496a62f8
BetterMe <betterme@cmadmins-MacBook-Air.local>
2021-03-18 15:15:14 +0200

    feat: added scroll function

---
+++ src/components/Container/PriceSection/Price/Price.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Price.module.css'
+
+const Price = ({onClickCallBack}) => {
+
+    return (
+        <div className={style.price}>
+
+        </div>
+    )
+};
+
+export default Price;
