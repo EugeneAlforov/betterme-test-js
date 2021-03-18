fecfa7f30d3159ca6017bf96fa96b91cbaa88b2d
BetterMe <betterme@cmadmins-MacBook-Air.local>
2021-03-18 14:29:26 +0200

    feat: added common button

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Container.module.css';
+import PriceSection from './PriceSection/PriceSection';
 
 const Main  = () => {
     return (
         <div className={style.containerInner}>
-            content
+            <PriceSection />
         </div>
     )
 }
