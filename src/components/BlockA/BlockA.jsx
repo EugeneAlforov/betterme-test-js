dbd85354f41a96918235a6ea33de7a95e910ffbf
arthurseredaa <neonbetcy@gmail.com>
2021-03-30 13:06:16 +0300

    Basic template

--- src/components/BlockA/BlockA.jsx
+++ src/components/BlockA/BlockA.jsx
@@ -1,7 +1,18 @@
+import Button from "../common/Button/Button"
+import classes from "./BlockA.module.css"
+
 export const BlockA = () => {
     return (
-        <div>
-            BlockA
+        <div className={classes.wrapper}>
+            <div className={classes.content}>
+                <div>
+                    Discount expires at
+                </div>
+                <div>
+                    cards
+                </div>
+                <Button handleClick={() =>console.log("test")} />
+            </div>
         </div>
     )
 }
