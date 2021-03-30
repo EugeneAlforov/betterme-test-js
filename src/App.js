920abf08ffd5fe2b4b772537076b4010e674da9b
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2021-03-30 13:25:14 +0300

    finished Block C

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import Test from "./components/test";
 import BlockC from "./components/BlockC/BlockC";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Test />
         <BlockC />
       </div>
     </Provider>
