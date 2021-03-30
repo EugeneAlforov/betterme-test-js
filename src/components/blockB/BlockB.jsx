182b5cae2844062fadf1c386464cc114b3faf311
BetterMe <betterme@cmadmin-Air17.local>
2021-03-30 14:39:22 +0300

    Block B logic ready

--- src/components/blockB/BlockB.jsx
+++ src/components/blockB/BlockB.jsx
@@ -1,25 +1,44 @@
-import React from "react"
+import React, {useState} from "react"
 import Button from "../common/Button/Button"
+import Input from "./Input"
 import "./BlockB.css"
 import Illustration from "../../assets/img/Illustration.png"
 
 const BlockB = () => {
-    const test = 'test';
+
+    const [showInput, setShowInput] = useState(true);
+    const [inputValue, setInputValue] = useState("")
+    
+    const showInputBlock = () => {
+        setShowInput(true);
+    }
+
+    const setToLocalStorage = () => {
+        localStorage.setItem("userEmail", JSON.stringify(inputValue));
+        setShowInput(false);
+        setInputValue("")
+    }
+
+    const setOnClick = () => {
+        return showInput
+                    ? setToLocalStorage
+                    : showInputBlock;
+    }
+
     return (
-    <div className="section-b">
+    <div className="section-b" id="blockB">
         <div className="b-left">
             <div className="text">
                 Get your <span>Personalized</span> Plan
             </div>
-            <div className="input">
-                <input type="text" placeholder="Enter your email to get your plan"/>
-            </div>
-            <Button />
+            
+            {showInput && <Input inputValue={inputValue} setInputValue={setInputValue} handleClick={() => setShowInput(false)}/>}
+            
+            <Button handleClick={setOnClick()}/>
         </div>
         <div className="b-right">
             <img src={Illustration} alt="" />
         </div>
-        
     </div>
     )
 }
--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,7 @@
 import s from "./Button.module.css";
 
 const Button = ({ handleClick }) => {
-  return <button className={s.button}>GET MY PLAN</button>;
+  return <button className={s.button} onClick={handleClick}>GET MY PLAN</button>;
 };
 
 export default Button;
