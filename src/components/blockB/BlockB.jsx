1ad7a018dc7246d429f6c5aa749894729d74452a
BetterMe <betterme@cmadmin-Air17.local>
2021-03-30 15:29:01 +0300

    Block B finished

--- src/components/blockB/BlockB.jsx
+++ src/components/blockB/BlockB.jsx
@@ -6,39 +6,43 @@ import Illustration from "../../assets/img/Illustration.png"
 
 const BlockB = () => {
 
-    const [showInput, setShowInput] = useState(true);
+    const [showInput, setShowInput] = useState(false);
     const [inputValue, setInputValue] = useState("")
     
-    const showInputBlock = () => {
-        setShowInput(true);
+    const toggleInputBlock = (val) => {
+        setShowInput(val);
     }
 
     const setToLocalStorage = () => {
         localStorage.setItem("userEmail", JSON.stringify(inputValue));
-        setShowInput(false);
+        toggleInputBlock(false);
         setInputValue("")
     }
 
     const setOnClick = () => {
         return showInput
                     ? setToLocalStorage
-                    : showInputBlock;
+                    : () => toggleInputBlock(true);
     }
 
     return (
     <div className="section-b" id="blockB">
         <div className="b-left">
+
             <div className="text">
                 Get your <span>Personalized</span> Plan
             </div>
             
-            {showInput && <Input inputValue={inputValue} setInputValue={setInputValue} handleClick={() => setShowInput(false)}/>}
+            {showInput && <Input inputValue={inputValue} setInputValue={setInputValue} handleClick={() => toggleInputBlock(false)}/>}
             
             <Button handleClick={setOnClick()}/>
+
         </div>
+
         <div className="b-right">
             <img src={Illustration} alt="" />
         </div>
+        
     </div>
     )
 }
--- src/components/blockB/Input.jsx
+++ src/components/blockB/Input.jsx
@@ -1,20 +1,28 @@
-import React from "react"
+import React, {useRef, useEffect} from "react"
 import close from "../../assets/img/Close.svg"
 
 const Input = ({inputValue, setInputValue, handleClick}) => {
 
+
+    const inputRef = useRef();
+    useEffect(() => {
+        inputRef.current.focus();
+    })
+
     return (
-        <div className="input">
-            <div className="input-left">
+        <>
+            <input 
+                ref={inputRef}
+                type="text" 
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <div className="input-controls">
                 <span>Enter your email to get your plan</span>
-                <input 
-                    type="text" 
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
+                <img onClick={handleClick} className="close-button" src={close} alt="close" />
             </div>
-            <img onClick={handleClick} className="close-button" src={close} alc="close" />
-        </div>
+        </>
+        
     )
     
 }
