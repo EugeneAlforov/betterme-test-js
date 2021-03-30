4c94614926905df9291753876fdb410160847bf2
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2021-03-30 12:02:04 +0300

    addedButton

---
+++ src/components/BlockC/BlockC.jsx
@@ -0,0 +1,16 @@
+import s from "./BlockC.module.css";
+import Button from "../common/Button/Button";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>
+        Reserved <span>75% discount</span> for:
+      </h1>
+      <p>09:59 minutes seconds</p>
+      <Button />
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/Button/Button.jsx
@@ -0,0 +1,7 @@
+import s from "./Button.module.css";
+
+const Button = () => {
+  return <button className={s.button}>GET MY PLAN</button>;
+};
+
+export default Button;
