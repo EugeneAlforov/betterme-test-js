6f95e96eed80d302d74393beec0f75bbe01dcefd
Vlad iamklim <drkleem@gmail.com>
2022-01-27 14:50:34 +0200

    Revert "Add personalized plan page"

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Test from './components/test';
-import PersonalizedPlanContainer from './containers/PersonalizedPlanContainer/PersonalizedPlanContainer'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/* <Test /> */}
-        <PersonalizedPlanContainer />
+        <Test />
       </div>
     </Provider>
   );
